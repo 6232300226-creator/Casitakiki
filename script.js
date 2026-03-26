558f4781c123526499425f332f5248c4d453faf2
6232300226-creator <6232300226@univermilenium.edu.mx>
2026-03-26 14:06:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ _supabase.auth.onAuthStateChange((event, session) => {
     if (session) {
         actualizarBotonUsuario(session.user);
     }
-});
+);
 
 verificarSesion();
 // =============================================
